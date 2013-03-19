0aacd5ed1c8335dc065696eb81da1222ec597998
veganist <veganist@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-19 10:37:24 +0000

    initial import
    git-svn-id: https://plugins.svn.wordpress.org/very-basic-content-restriction/trunk@684148 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== Very basic content restriction ===
+Contributors: veganist
+Donate link:
+Tags: content restriction
+Requires at least: 3.3
+Tested up to: 3.5.1
+Stable tag:
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Restricts access to all content except pages for non autheticated users.
+
+== Description ==
+
+This plugin redirects non authenticated users to a page of your choice or, as per default, to the login page.
+Posts, categories, feeds, tags, taxonomies, author pages and search results are restricted to authenticated (connected) users.
+Only pages are public. So if you have a homepage defined, then this page will be accessible.
+
+== Installation ==
+
+1. Upload `vers_basic_content_restrictions` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Configure the redirection link on the Options page. If left empty, users will be redirected to WP login.
+
+== Frequently asked questions ==
+
+Q: Can i restrict only some pages and allow others?
+A: No, a per page or per category configuration is not possible.
+
+== Changelog ==
+
+1. 0 Initial release
---
+++ very_basic_content_restriction.php
@@ -0,0 +1,86 @@
+<?php
+/*
+Plugin Name: Very Basic Content Restriction
+Plugin URI: http://curlybracket.net/plugz/vbcr
+Description: Restrict access to categories, posts, tags, taxonomies, feeds. Allow only access to pages. If user is not connected, redirect to login page.
+Version: 1.0
+Author: Ulrike Uhlig
+Author URI: http://curlybracket.net
+License: GPL2
+*/
+/*
+    Copyright 2012  Ulrike Uhlig (email : u@curlybracket.net)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
+<?
+
+function vbcr_simple_content_restriction() {
+    global $wp_query;
+    if( !is_user_logged_in() ) {
+        if( is_category() || is_single() || is_tax() || is_tag() || is_feed() || is_comment_feed() || is_attachment() || is_search() ) {
+            $option_redirect_to = get_option('redirect-to');
+            if(empty($option_redirect_to)) {
+                $option_redirect_to = get_bloginfo('url')."/wp-login.php";
+            }
+            $status = "302";
+            wp_redirect( $option_redirect_to, $status ); exit;
+        }
+    }
+    ob_flush();
+}
+
+function content_restriction_output_buffer() {
+	// this is needed for the redirection
+    ob_start();
+}
+
+add_action('init', 'content_restriction_output_buffer');
+add_action('pre_get_posts', 'vbcr_simple_content_restriction');
+
+/**
+ * Add options page to settings menu
+ */
+
+if ( is_admin() ){ // admin actions
+    add_action('admin_menu', 'wp_content_restriction_menu');
+}
+
+function wp_content_restriction_menu() {
+    add_submenu_page( 'options-general.php', 'Content Restriction', 'Content Restriction', 'manage_options', 'content_restriction_page', 'content_restriction_page_callback' );
+    add_action( 'admin_init', 'register_content_restriction_settings' );
+}
+
+function content_restriction_page_callback() {
+    echo '<div class="wrap">';
+    screen_icon();
+    echo '<h2>Content Restriction</h2>';
+    echo '<form method="post" action="options.php">';
+    settings_fields( 'content-restriction-group' );
+    echo '<label>Redirect to (absolute URL) <input type="text" name="redirect-to" value="'.get_option('redirect-to').'" /></label>';
+    echo '<p>If this field is empty, we will redirect people to the login page of your blog.</p>';
+    submit_button();
+    echo '</form></div>';
+}
+
+function register_content_restriction_settings() {
+    register_setting( 'content-restriction-group', 'redirect-to', 'vbcr_sanitize' );
+}
+
+function vbcr_sanitize($input) {
+	$input = sanitize_text_field($input);
+	return $input;
+}
+?>
